c206a9003643b64c8e08bc84a2bed01ff8344b78
FerloDeveloper <gianluca.laferla.dev@gmail.com>
2023-05-07 18:18:54 +0200

    FizzBuzz

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
 const container = document.querySelector(".container");
 
-for (let i = 1; i <= 100; i++){
+for (let i = 1; i <= 100; i++) {
+	const element = document.createElement("div");
 
-    const element = document.createElement("div");
+	element.classList.add("box");
 
-    element.classList.add("box");
-
-    if (i % 3 === 0 && i % 5 === 0){
-        element.innerText = "FizzBuzz";
-        element.classList.add("red");
-        container.append(element);
-        console.log("FizzBuzz");
-
-    } else if(i % 5 === 0){
-        element.innerText = "Buzz";
-        element.classList.add("yellow")
-        container.append(element);
-        console.log("Buzz");
-    } else if(i % 3 === 0){
-        element.innerText = "Fizz";
-        element.classList.add("green")
-        container.append(element);
-        console.log("Fizz");
-    } else {
-        element.innerText = i;
-        element.classList.add("blue")
-        container.append(element);
-        console.log(i);
-    }
+	if (i % 3 === 0 && i % 5 === 0) {
+		element.innerText = "FizzBuzz";
+		container.append(element);
+		element.classList.add("red");
+		console.log("FizzBuzz");
+	} else if (i % 5 === 0) {
+		element.innerText = "Buzz";
+		container.append(element);
+		element.classList.add("yellow");
+		console.log("Buzz");
+	} else if (i % 3 === 0) {
+		element.innerText = "Fizz";
+		container.append(element);
+		element.classList.add("green");
+		console.log("Fizz");
+	} else {
+		element.innerText = i;
+		container.append(element);
+		element.classList.add("blue");
+		console.log(i);
+	}
 }
-
-
